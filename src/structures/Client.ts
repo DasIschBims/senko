4ee76e2713d92c7c2887a9bc0f1128928100185a
DasIschBims <46683337+DasIschBims@users.noreply.github.com>
2022-07-26 10:58:43 +0200

    Update Client.ts

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -44,10 +44,10 @@ export class ExtendedClient extends Client {
         app.use(function(req, res) {
            res.status(404);
         
-           if (req.accepts("html")) {
-              res.render("404", { url: req.url });
-              return;
-           }
+//           if (req.accepts("html")) {
+//              res.render("404", { url: req.url });
+//              return;
+//           }
 
            if (req.accepts("json")) {
               res.json({ error: "Not found" });
