4b37f1f3e031d8c92c5ddaeedbd2e68d25140e84
DasIschBims <gilmann.florian@gmail.com>
2022-07-26 23:28:01 +0200

    check other users level

--- src/Commands/utility/level.ts
+++ src/Commands/utility/level.ts
@@ -1,4 +1,4 @@
-import { EmbedBuilder } from "discord.js";
+import { EmbedBuilder, ApplicationCommandOptionType } from "discord.js";
 import { Command } from "../../structures/Command";
 import profileSchema from "../../schemas/profile";
 import mongoose from "mongoose";
@@ -6,33 +6,64 @@ import mongoose from "mongoose";
 export default new Command({
     name: "level",
     description: "Get your current level and XP",
+    options: [
+        {
+            name: "user",
+            description: "Get the level of another user",
+            type: ApplicationCommandOptionType.User,
+            required: false,
+        }
+    ],
     run: async ({ interaction }) => {
         const mongo = async () => await mongoose.connect(process.env.mongodbUri);
 
         await mongo().then(async (db) => {
             try {
-                const profile = await profileSchema.findOne({
-                    guildId: interaction.guild.id,
-                    userId: interaction.user.id,
-                })
+                var profile;
+                if (interaction.options.get("user")) {
+                    const user = interaction.options.get("user");
+                    profile = await profileSchema.findOne({
+                        userId: user.user.id,
+                        guildId: interaction.guild.id,
+                    });
+                } else {
+                    profile = await profileSchema.findOne({
+                        guildId: interaction.guild.id,
+                        userId: interaction.user.id,
+                    });
+                }
 
                 if (!profile) {
                     await profileSchema.create({
                         userId: interaction.user.id,
                         guildId: interaction.guild.id,
                     });
-
-                    return interaction.followUp({
-                        embeds: [
-                            new EmbedBuilder()
-                            .setColor(`#${process.env.embedColor}`)
-                            .setAuthor({
-                                name: `${interaction.user.username}#${interaction.user.discriminator}`,
-                                iconURL: interaction.user.avatarURL({ size: 2048, forceStatic: false }),
-                            })
-                            .setDescription("You didn't have a profile yet, so I created one for you.")
-                        ]
-                    })
+                    if (interaction.options.get("user")) {
+                        const user = interaction.options.get("user");
+                        return interaction.followUp({
+                            embeds: [
+                                new EmbedBuilder()
+                                    .setColor(`#${process.env.embedColor}`)
+                                    .setAuthor({
+                                        name: `${user.user.username}#${user.user.discriminator}`,
+                                        iconURL: user.user.avatarURL({ size: 2048, forceStatic: false }),
+                                    })
+                                    .setDescription("This user has no profile yet.")
+                            ]
+                        })
+                    } else {
+                        return interaction.followUp({
+                            embeds: [
+                                new EmbedBuilder()
+                                    .setColor(`#${process.env.embedColor}`)
+                                    .setAuthor({
+                                        name: `${interaction.user.username}#${interaction.user.discriminator}`,
+                                        iconURL: interaction.user.avatarURL({ size: 2048, forceStatic: false }),
+                                    })
+                                    .setDescription("You didn't have a profile yet, so I created one for you.")
+                            ]
+                        })
+                    }
                 }
 
                 interaction.followUp({ embeds: [
